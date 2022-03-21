fbb5ceab57eb43303f8cec986a6c7bbf5c67e25a
Ivan <Bospur@mail.ru>
2022-03-21 23:49:53 +0500

    new

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 let headerItem = document.getElementById('header_menu');
 let headerlist = document.querySelector('.header__list');
+const sliderContainer = document.querySelector('.slider__row');
+const sliders = sliderContainer.querySelectorAll('.slider__slide');
+const arrowLeft = document.querySelector('._arrow_left');
+const arrowRight = document.querySelector('._arrow_right');
 
 headerItem.addEventListener('click', () => {
     headerlist.classList.toggle('display-block');
 })
+
+arrowLeft.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    sliders[0].style.marginRight = (-100) + '%';
+});
+
+arrowRight.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    sliders[0].style.marginRight = (100) + '%';
+});
